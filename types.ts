8ccd8ebc3456cf5d3225455393819fbcd1369686
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-08-28 06:13:43 -0300

    +types.ts: reduce to 1 import needed on every .ts

---
+++ types.ts
@@ -0,0 +1,11 @@
+import { App } from "./app.ts";
+import { assertEquals } from "https://deno.land/std@0.106.0/testing/asserts.ts";
+import { Sum } from "./server/sum.ts";
+import { Subtraction } from "./server/subtraction.ts";
+
+export {
+  assertEquals,
+  App,
+  Sum,
+  Subtraction
+};
