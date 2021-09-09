1b874a12fa779feb0b2dd86c8c9cd51510c8083b
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-09-09 18:35:14 -0300

    feat history: proper handle socket events
    and execute client calcs, return results to client

--- server/history.ts
+++ server/history.ts
@@ -1,7 +1,22 @@
-import { WebSocket } from "https://deno.land/std@0.106.0/ws/mod.ts";
+import { WebSocket, isWebSocketCloseEvent } from "https://deno.land/std@0.106.0/ws/mod.ts";
+import { Calculator } from "../types.ts";
 
 export class History{
-  static async run(ws: WebSocket){
+  static async run(serverSocket: WebSocket){
     console.log("Novo socket se conectou!");
+
+    for await (const event of serverSocket){
+      if(isWebSocketCloseEvent(event)) break;
+  
+      if(typeof event === "string"){
+        let args = event.split(" ");
+        if(args[0] == "")  args.shift();
+        if(args[0] == "√") args = [args[1], "√", "2"];
+
+        let result = Calculator.calc( args );
+        if( result.startsWith("E") ) result = "error";
+        serverSocket.send( result );
+      }
+    }
   }
 }
