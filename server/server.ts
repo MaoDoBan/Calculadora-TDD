a8691b5444d65d914ebfb0724cb46a0de741fa33
MaoDoBan <11332249+MaoDoBan@users.noreply.github.com>
2021-09-09 18:30:38 -0300

    refactor server: only code segregation

--- server/server.ts
+++ server/server.ts
@@ -1,4 +1,4 @@
-import { serve }     from "https://deno.land/std@0.106.0/http/server.ts";
+import { serve, ServerRequest }     from "https://deno.land/std@0.106.0/http/server.ts";
 import { serveFile } from "https://deno.land/std@0.106.0/http/file_server.ts";
 import { acceptWebSocket, acceptable } from "https://deno.land/std@0.106.0/ws/mod.ts";
 import { History } from "./history.ts";
@@ -9,24 +9,31 @@ export class Server{
     console.log("Server running on: http://localhost:8000/");
 
     for await (const request of server) {
-      try{
-        if(request.url === "/ws"){
-          if(!acceptable(request)) continue;
-          acceptWebSocket({
-            conn: request.conn,
-            bufReader: request.r,
-            bufWriter: request.w,
-            headers: request.headers
-          })
-          .then(History.run);
-          continue;
-        }
-        console.log("path:",request.url);
-        const content = await serveFile(request, `./client${request.url}`);
-        request.respond(content);
-      }catch(error){
-        console.error(error);
-      }
+      Server.process(request);
     }
+  }
+
+  private static async process(request: ServerRequest){
+    try{
+      if(request.url === "/ws") return Server.validateWebSocket(request);
+      
+      console.log("path:", request.url);
+      const content = await serveFile(request, `./client${request.url}`);
+      request.respond(content);
+    }catch(error){
+      console.error(error);
+    }
+  }
+
+  private static validateWebSocket(request: ServerRequest){
+    if(!acceptable(request)) return;
+
+    acceptWebSocket({
+      conn: request.conn,
+      bufReader: request.r,
+      bufWriter: request.w,
+      headers: request.headers
+    })
+    .then(History.run);
   }
 }
